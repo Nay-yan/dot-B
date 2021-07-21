cefc496c3cd68e90d366786ae853679366299df5
WaiYan <aa8184792@gmial.com>
2021-07-21 14:36:43 +0630

    first

--- KNOWLEDGE/S-Post/js/app.js
+++ KNOWLEDGE/S-Post/js/app.js
@@ -3,4 +3,35 @@ $(document).ready(function() {
         $(this).toggleClass('open');
     });
     document.getElementById("year").innerHTML = new Date().getFullYear();
+
+    //scroll Nav
+    let screenHeight = $(window).height();
+    // console.log(screenHeight);
+    $(window).scroll(function() {
+        let scrollHeight = $(this).scrollTop();
+        // console.log(scrollHeight);
+
+        if (scrollHeight >= screenHeight - 500) {
+            $('.navbar-light').addClass("scrollNav-none");
+        } else {
+            $('.navbar-light').removeClass("scrollNav-none");
+
+        }
+    });
 });
+
+var scrollBtn = document.getElementById("scrollTop");
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
